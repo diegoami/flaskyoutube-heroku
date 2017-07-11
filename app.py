24cbbb9168cbfa8e94b9c914e2b79273da27e846
Diego Amicabile <diego.amicabile@gmail.com>
2017-07-11 22:08:42 +0200

    Session problem fixed (heroku)

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ from oauth2client import client
 
 
 app = flask.Flask(__name__)
-import uuid
 
-app.secret_key = str(uuid.uuid4())
+app.secret_key = 'E8OusFzWtzQTE72RvcHv'
 
 YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
 YOUTUBE_API_SERVICE_NAME = "youtube"
@@ -21,13 +20,23 @@ YOUTUBE_API_VERSION = "v3"
 
 
 def execute_command(http_auth ):
-  youtube = Youtube(discovery.build(YOUTUBE_API_SERVICE_NAME , YOUTUBE_API_VERSION, http_auth))
+  youtube = Youtube(discovery.build(YOUTUBE_API_SERVICE_NAME , YOUTUBE_API_VERSION, http=http_auth))
   likedchannel = youtube.liked_channel()
   videos1 = youtube.videos_in_channels(likedchannel)
   videos2 = youtube.videos_in_channels(likedchannel, videos1['nextPageToken'] if 'nextPageToken' in videos1 else None )
   items = list(videos1.values()) + list(videos2.values())
   return json.dumps(items )
 
+
+@app.route('/reset')
+def reset():
+    if 'credential' in flask.session:
+        del flask.session['credentials']
+    return ('', 204)
+
+@app.route('/go')
+def go():
+    return flask.redirect(flask.url_for('oauth2callback'))
 
 @app.route('/')
 def index():
@@ -61,7 +70,6 @@ def oauth2callback():
 
 
 if __name__ == '__main__':
-  import uuid
-  app.secret_key = str(uuid.uuid4())
+  app.secret_key = 'E8OusFzWtzQTE72RvcHv'
   app.debug = False
   app.run()
